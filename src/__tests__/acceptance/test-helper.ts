c72890e05702b0ddda305808aa998dd1ead8c6bd
Thomah <5090230+Thomah@users.noreply.github.com>
2020-11-28 11:27:24 +0100

    Use localhost IP for tests

--- src/__tests__/acceptance/test-helper.ts
+++ src/__tests__/acceptance/test-helper.ts
@@ -10,7 +10,7 @@ export async function setupApplication(): Promise<AppWithClient> {
     // Customize the server configuration here.
     // Empty values (undefined, '') will be ignored by the helper.
     //
-    host: 'localhost',
+    host: '127.0.0.1',
     port: 3000,
   });
 
